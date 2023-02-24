7624072a788c6b5c95728e10f2285096741da807
wonil <https://github.com/imwonil/llllll-dd.git>
2023-02-24 20:01:06 +0900

    Add Get Customers

---
+++ client/src/setupProxy.js
@@ -0,0 +1,13 @@
+
+
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/api", //추가'/api/customers'
+    createProxyMiddleware({
+      target: "http://localhost:5000",
+      changeOrigin: true,
+    })
+  );
+};
